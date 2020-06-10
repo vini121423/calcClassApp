54b31f67e7601db7f093a87a232265bea84e45a2
vini121423 <vinicius.santos838@etec.sp.gov.br>
2020-06-10 12:10:36 -0300

    Aulas de Ionic, produzindo uma calculadora

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
@@ -12,8 +12,9 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
     Tab1PageRoutingModule
+
+    // Excluir explorer container //
   ],
   declarations: [Tab1Page]
 })
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+   
+   public primeiroValor:number;
+   public segundoValor:number;
+
+   public resultado:number;
 
   constructor() {}
+   public somar(){
+     this.resultado = this.primeiroValor + this.segundoValor;
+   }
 
+   public subtrair(){
+    this.resultado = this.primeiroValor - this.segundoValor;
+  }
+
+  public dividir(){
+    this.resultado = this.primeiroValor / this.segundoValor;
+  }
+
+  public multiplicar(){
+    this.resultado = this.primeiroValor * this.segundoValor;
+  }
+
+  public limpar(){
+    this.resultado = null;
+    this.primeiroValor = null;
+    this.segundoValor = null;
+  }
 }
