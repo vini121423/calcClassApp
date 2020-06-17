5a476aec3b204fe2bfa1cc55a61dec81da5b067f
vini121423 <vinicius.santos838@etec.sp.gov.br>
2020-06-17 15:12:49 -0300

    Criação da calculadora normal, layout e pequenas ações

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+    public calculo = '';
+    public resultado: string;
 
   constructor() {}
+    
+    public adicionarNumero(valor:string){
+      this.calculo = this.calculo + valor;
+    }
 
+    public adicionarPonto(){
+      // Mesma forma, só que resumida //
+      this.calculo += ".";
+    }
+
+    public adicionarOperacao(operador: string){
+     this.calculo += operador;
+    }
+
+    public apagarTudo(){
+      this.calculo = '';
+      this.resultado = null;
+    }
+
+    public apagarUltimo(){
+      this.calculo = this.calculo.substring(0,(this.calculo.length -1));
+    }
 }
